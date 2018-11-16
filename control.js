1dc6f4c5be95da0f893eabe8e8c4375d64eb112a
Vladimir Zhuravlev <izhur27@gmail.com>
2018-11-16 11:45:13 +0100

    Initial commit

---
+++ control.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.addEventListener("mousedown", mousedownHandler);
+    document.body.addEventListener("mouseup", mouseupHandler);
+});
+
+var lastTouch;
+function mousedownHandler(event) {
+    lastTouch = [event.clientX, event.clientY];
+}
+
+function mouseupHandler (event) {
+    var positionX = (event.clientX + lastTouch[0]) / 2;
+    var deltaY = event.clientY - lastTouch[1];
+
+    moveWolf(positionX > document.body.getBoundingClientRect().width / 2, deltaY > 0);
+}
+
+
+document.body.addEventListener("touchmove", fieldTouchHandler);
+
+function fieldTouchHandler(event) {
+    var x,y;
+
+    x = event.clientX;
+    y = event.clientY;
+
+    if (!(x && y)) {
+        x = event.changedTouches[0].clientX;
+        y = event.changedTouches[0].clientY;
+    } else if (!beingMoved) {
+        return;
+    }
+
+    if (lastTouch) {
+        var positionX = (x + lastTouch[0]) / 2;
+        var deltaY = y - lastTouch[1];
+        moveWolf(positionX > document.body.getBoundingClientRect().width / 2, deltaY > 0);
+    }
+
+    lastTouch = [x,y];
+
+}
+
---
+++ game.js
@@ -0,0 +1,44 @@
+var gameData = [
+    { "egg": 1, "value": 200 },
+    { "egg": 2, "value": 200 },
+    { "egg": 3, "value": -300 },
+    { "egg": 2, "value": 200 }
+];
+var gameCounter = 0;
+var pointsCounter = 0;
+
+function gameStep() {
+    var step = gameData[gameCounter];
+    moveEgg(step.egg);
+    if (step.value > 0) {
+        eggsNodes[step.egg].classList.remove("bad");
+    } else {
+        eggsNodes[step.egg].classList.add("bad");
+    }
+
+    window.setTimeout(function() {
+        if (correctPlayerLocation(step)) {
+            console.log("ok");
+            pointsCounter += step.value;
+            pointsCounterNode.innerText = pointsCounter;
+            prizeNode.innerText = step.value + "$";
+            flyPrize(step.value > 0);
+        } else {
+            console.log("loch");
+        }
+        gameCounter++;
+        if (gameCounter >= gameData.length) {
+            gameCounter = 0;
+        }
+        gameStep();
+    }, eggMoveDuration*1000);
+}
+
+function correctPlayerLocation(step) {
+    if (wolfLocation[0] == eggsData[step.egg].isRight && wolfLocation[1] == eggsData[step.egg].isBottom) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
